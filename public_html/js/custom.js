38fa011c94ab5558780f823f229a9728c1c1b850
netoiung <neto_anibal@hotmail.com>
2016-09-20 05:46:36 -0300

    Site bombando

--- public_html/js/custom.js
+++ public_html/js/custom.js
@@ -97,8 +97,8 @@ $(function(){
   $(function(){
     jQuery(document).ready(function() {
     $('#home').backstretch([
-       "images/maxresdefault.jpg", 
-       "images/home-bg-slideshow2.jpg",
+       "images/maxresdefault.jpg",
+       "images/back3.jpg",
         ],  {duration: 2000, fade: 750});
     });
   })
